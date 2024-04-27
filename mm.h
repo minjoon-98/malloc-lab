de2359c18d2f7c957621216c7dcbb594d80aab0d
minjoon-98 <4kmj54321@gmail.com>
2024-04-27 22:05:15 +0900

    240427 ImplicitFreeList FirstFit except mm_realloc

--- mm.h
+++ mm.h
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-extern int mm_init (void);
-extern void *mm_malloc (size_t size);
-extern void mm_free (void *ptr);
+extern int mm_init(void);
+extern void *mm_malloc(size_t size);
+extern void mm_free(void *ptr);
 extern void *mm_realloc(void *ptr, size_t size);
 
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
 
-/* 
- * Students work in teams of one or two.  Teams enter their team name, 
+/*
+ * Students work in teams of one or two.  Teams enter their team name,
  * personal names and login IDs in a struct of this
  * type in their bits.c file.
  */
-typedef struct {
+typedef struct
+{
     char *teamname; /* ID1+ID2 or ID1 */
     char *name1;    /* full name of first member */
     char *id1;      /* login ID of first member */
@@ -20,4 +25,3 @@ typedef struct {
 } team_t;
 
 extern team_t team;
-
